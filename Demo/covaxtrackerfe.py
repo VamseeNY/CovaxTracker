aa12247faf4a0e09d8f0edc42517cd89c510d1a6
Vamsee <104006094+VamseeNY@users.noreply.github.com>
2022-11-12 16:50:38 +0530

    Update covaxtrackerfe.py

--- Demo/covaxtrackerfe.py
+++ Demo/covaxtrackerfe.py
@@ -11,25 +11,14 @@ fig=Figure(width=550,height=850)
 m=folium.Map(location=[21,80],zoom_start=5,tiles='cartodbpositron')#cartodbdark_matter
 fig.add_child(m)
 
+
 data=ast.literal_eval(st.secrets["fin"])
-
-geolocator = Nominatim(user_agent="MyApp")
-
-latlong={}
-n=[]
 loc=list(data.keys())
-for i in loc:
-    location = geolocator.geocode(i)
-    try:
-        latlong[i]=[location.latitude,location.longitude]
-    except AttributeError :
-        n.append(i) #use a different engine for these locations
+
+latlong=ast.literal_eval(st.secrets["coordinates"])
 
 
 for i in loc:
     folium.Marker(latlong[i],popup=data[i],tooltip=i).add_to(m)
 
 folium_static(m,width=550,height=850)
-
-
-
